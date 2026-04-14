1ce1cd277bf5b045f15f96337d415508fca2ebe3
ThunderJD <jdliu688@gmail.com>
2026-04-14 15:43:22 +0000

    Day 1 of css

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // add javascript here
+function replace(){
+    location.replace("https://krazysmash.com/pages/greatest-badminton-players?srsltid=AfmBOoqids7oAOWsZUGyP7vn6gi6CpAl7tcvf5Xrskp9KIVby6RRWtbU")
+}
