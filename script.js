ada9e5afe35bfbc03971fb1a2c8199a74e4e2117
ThunderJD <jdliu688@gmail.com>
2026-04-14 16:47:30 +0000

    Day 1 and 2 of JS

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 function replace(){
     location.replace("https://krazysmash.com/pages/greatest-badminton-players?srsltid=AfmBOoqids7oAOWsZUGyP7vn6gi6CpAl7tcvf5Xrskp9KIVby6RRWtbU")
 }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function hide() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
