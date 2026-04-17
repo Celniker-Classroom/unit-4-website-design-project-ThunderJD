7269ad779a1732730f4a07e8dba5de0bf0a6586b
ThunderJD <jdliu688@gmail.com>
2026-04-17 16:56:04 +0000

    Might be final website (TBD)

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 function replace(){
     location.replace("https://krazysmash.com/pages/greatest-badminton-players?srsltid=AfmBOoqids7oAOWsZUGyP7vn6gi6CpAl7tcvf5Xrskp9KIVby6RRWtbU")
 }
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
